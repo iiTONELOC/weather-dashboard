f2c6cf3ae0c8b55da624d8e26bc353de87b38a45
iiTONELOC <anthonytropeano@me.com>
2021-01-28 16:38:56 -0500

    clear page on landing, show message only

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var currentCityEl= document.getElementById('currentCity');
+var titleContainerEl=document.getElementById('title-container');
+var forecastCardContainerEl=document.getElementById('forecastCards');
+var landingMessageEl=document.getElementById('landingMessage');
+
+//show landing message only on load
+currentCityEl.style.visibility="hidden";
+titleContainerEl.style.visibility="hidden";
+forecastCardContainerEl.style.visibility="hidden";
