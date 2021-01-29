8474e807215974a82389cc9694ea0ab8be6d61d6
iiTONELOC <anthonytropeano@me.com>
2021-01-29 00:54:49 -0500

    show hidden divs on button click, hide landing message

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,7 +11,7 @@ currentCityEl.style.visibility="hidden";
 titleContainerEl.style.visibility="hidden";
 forecastCardContainerEl.style.visibility="hidden";
 
-// city list function
+// load localStorage append items to page
 var loadMemory=function(){
     var memory= JSON.parse(localStorage.getItem("storage"));
 
@@ -30,7 +30,7 @@ var loadMemory=function(){
         console.log(storage);
     }
 }
-
+//save data to local storage 
 var save=function(){
 
     localStorage.setItem("storage", JSON.stringify(storage));
@@ -43,7 +43,13 @@ var getWeather= function(event){
     console.log(event.target);
     var location=document.querySelector("input").value.trim();
     console.log(location);
+    //clear input
     document.querySelector("input").value="";
+    //hide landing message show rest of page
+    currentCityEl.style.visibility="visible";
+    titleContainerEl.style.visibility="visible";
+    forecastCardContainerEl.style.visibility="visible";
+    landingMessageEl.style.visibility="hidden";
     
     fetch("https://api.openweathermap.org/data/2.5/weather?q="+location+"&units=imperial&appid="+"339a47de31fdebaebb64d0a528d98345")
     .then(function(response){
@@ -55,6 +61,7 @@ var getWeather= function(event){
         
         console.log(data);
         var cityName=data.name;
+        //check for duplicates before saving
         var duplicate = false;
         for (let i = 0; i < storage.length; i++) {
             const element = storage[i];
@@ -63,6 +70,7 @@ var getWeather= function(event){
             }
             
         }
+        //if not a duplicate, push cityName to storage array, save the array to localStorage, generate last searched item to page, 
         if(!duplicate){
             storage.push(cityName);
             save();
@@ -71,8 +79,9 @@ var getWeather= function(event){
             city.setAttribute("value",cityName);
             city.textContent=cityName;
             listGroup.appendChild(city);
-        }
-        
+            //display current weather data
+
+        }  
         
         
         
